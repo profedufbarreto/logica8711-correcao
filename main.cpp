f2800e26c926a4258b93ceb387883f70f19b399a
EDUARDO FILIPPSEN BARRETO <01289211027@senacrs.edu.br>
2026-04-13 22:15:15 -0300

    if else

--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include<iostream>
-#include<cmath>
 
 int main(){
     
-    int a = 5;
-    int b = 3;
+    int opcaoPagamento;
+    int qtdParafuso;
+    float valorParafuso = 1.40;
+    float valorTotalParafuso;
+    float desconto;
+    float juros;
 
-    std::cout<<std::min(a, b)<<std::endl;
+    std::cout<<"Digite a quantidade de parafusos desejada: "<<std::endl;
+    std::cin>>qtdParafuso;
 
+    valorTotalParafuso = qtdParafuso * valorParafuso;
+
+    std::cout<<"Qual a forma de pagamento: "<<std::endl;
+    std::cout<<"--- 1 - Pix"<<std::endl;
+    std::cout<<"--- 2 - Dinheiro"<<std::endl;
+    std::cout<<"--- 3 - Cartão"<<std::endl;
+    std::cin>>opcaoPagamento;
+
+    if(opcaoPagamento == 1){
+        std::cout<<"Sua compra ficou no valor total de R$: "<<valorTotalParafuso<<std::endl;
+    }else if(opcaoPagamento == 2){
+        desconto = valorTotalParafuso * 0.90;
+        std::cout<<"Sua compra ficou no valor total de R$: "<<desconto<<std::endl;
+    }else{
+        juros = valorTotalParafuso * 1.10;
+        std::cout<<"Sua compra ficou no valor total de R$: "<<valorTotalParafuso<<std::endl;
+    }
+
+ 
     return 0;
 }
