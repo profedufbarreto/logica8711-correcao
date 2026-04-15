076d68af2a26acfb75fc2c4d3b85211826a5d007
EDUARDO FILIPPSEN BARRETO <01289211027@senacrs.edu.br>
2026-04-15 19:48:38 -0300

    for

--- main.cpp
+++ main.cpp
@@ -1,35 +1,14 @@
 #include<iostream>
+#include<thread> //para lidar com o tempo de execução
+#include<chrono> // para definir unidades de tempo (ms, segundos)
 
 int main(){
-    
-    int opcaoPagamento;
-    int qtdParafuso;
-    float valorParafuso = 1.40;
-    float valorTotalParafuso;
-    float desconto;
-    float juros;
 
-    std::cout<<"Digite a quantidade de parafusos desejada: "<<std::endl;
-    std::cin>>qtdParafuso;
-
-    valorTotalParafuso = qtdParafuso * valorParafuso;
-
-    std::cout<<"Qual a forma de pagamento: "<<std::endl;
-    std::cout<<"--- 1 - Pix"<<std::endl;
-    std::cout<<"--- 2 - Dinheiro"<<std::endl;
-    std::cout<<"--- 3 - Cartão"<<std::endl;
-    std::cin>>opcaoPagamento;
-
-    if(opcaoPagamento == 1){
-        std::cout<<"Sua compra ficou no valor total de R$: "<<valorTotalParafuso<<std::endl;
-    }else if(opcaoPagamento == 2){
-        desconto = valorTotalParafuso * 0.90;
-        std::cout<<"Sua compra ficou no valor total de R$: "<<desconto<<std::endl;
-    }else{
-        juros = valorTotalParafuso * 1.10;
-        std::cout<<"Sua compra ficou no valor total de R$: "<<valorTotalParafuso<<std::endl;
+    for(int i = 10; i >= 0; i--){
+        std::cout<<"A bomba irá explodir em.."<<i<<std::endl;
     }
 
- 
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
     return 0;
 }
