0ed405bfc3bd9e63b3e3ffbc68e441639f87254f
EDUARDO FILIPPSEN BARRETO <01289211027@senacrs.edu.br>
2026-04-28 19:25:35 -0300

    break

--- main.cpp
+++ main.cpp
@@ -2,20 +2,12 @@
 
 int main(){
 
-    int num;
-
-    do{
-        std::cout<<"Digite um valor para num: "<<std::endl;
-        std::cin>>num;
-
-        if(num < 1 || num > 10){
-            std::cout<<"Entrada inválida!!"<<std::endl;
+    for(int i = 0; i < 10; i++){
+        if(i == 4){
+            break;
         }
-    }while(num < 1 || num > 10);{
-        std::cout<<"Obrigado!! Você escolheu o número: "<<num<<std::endl;
+        std::cout<<i<<std::endl;
     }
-
-
     
     return 0;
 }
