0994c9c7d9d080f727f6a6e67f8367a5ac5b3f7b
EDUARDO FILIPPSEN BARRETO <01289211027@senacrs.edu.br>
2026-04-28 21:34:16 -0300

    return value

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include<iostream>
-#include<string>
 
-void minhaFuncao(std::string nome){
-    std::cout<<nome<<std::endl;
+int myFunction(int x){
+    return 5 + x;
 }
 
 int main(){
 
-    minhaFuncao("Eduardo");
-    minhaFuncao("Nome");
+    std::cout<<myFunction(3)<<std::endl;
 
     return 0;
 }
