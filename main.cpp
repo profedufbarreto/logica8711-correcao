76a035922f2325d5d0c0cee89255a336b2316dbf
EDUARDO FILIPPSEN BARRETO <01289211027@senacrs.edu.br>
2026-04-30 22:01:56 -0300

    seconds

--- main.cpp
+++ main.cpp
@@ -1,27 +1,17 @@
 #include<iostream>
-#include<string>
 
 int main(){
 
-    int opcao;
-    std::string nome;
-    int idade;
+    int num;
+    std::cout<<"Digite um valor para num: "<<std::endl;
+    std::cin>>num;
 
-    std::cout<<"----- Escolha a opção adequada: "<<std::endl;
-    std::cout<<"Digite 1 para cadastrar ou 2 para sair: "<<std::endl;
-    std::cin>>opcao;
-
-    if(opcao == 1){
-        std::cout<<"Digite o nome: "<<std::endl;
-        std::cin>>nome;
-        std::cout<<"Digite a idade: "<<std::endl;
-        std::cin>>idade;
-
-        std::cout<<"Seu nome é: "<<nome<<std::endl;
-        std::cout<<"Sua idade é: "<<idade<<std::endl;
+    if(num % 2 == 0){
+        std::cout<<num<<" é par!"<<std::endl;
     }else{
-        std::cout<<"Até a próxima!"<<std::endl;
+        std::cout<<num<<" é ímpar!!"<<std::endl;
     }
 
+    
     return 0;
 }
