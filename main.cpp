b7396ccd874bb8d7c10730d8044d5ff858861864
EDUARDO FILIPPSEN BARRETO <01289211027@senacrs.edu.br>
2026-05-12 20:56:43 -0300

    calculadora com funções completa

--- main.cpp
+++ main.cpp
@@ -1,27 +1,68 @@
 #include<iostream>
 
+float somar(float a, float b){
+    return a + b;
+}
+
+float subtrair(float a, float b){
+    return a - b;
+}
+
+float multiplicar(float a, float b){
+    return a * b;
+}
+
+float dividir(float a, float b){
+    if(b == 0){
+        std::cout<<"Não é possível dividir por 0!"<<std::endl;
+        return 0;
+    }else{
+    return a / b;
+    }
+}
+
+void coletarNumeros(float &a, float &b){
+    std::cout<<"Digite um valor para a: "<<std::endl;
+    std::cin>>a;
+
+    std::cout<<"Digite um valor para b: "<<std::endl;
+    std::cin>>b;
+
+}
+
+void exibirResultado(float &a, float &b){
+    coletarNumeros(a, b);
+
+    int op;
+    std::cout<<"Digite uma opção: "<<std::endl;
+    std::cout<<"1 - Somar"<<std::endl;
+    std::cout<<"2 - Subtrair"<<std::endl;
+    std::cout<<"3 - Multiplicar"<<std::endl;
+    std::cout<<"4 - Dividir"<<std::endl;
+    std::cin>>op;
+
+    switch(op){
+        case 1:
+        std::cout<<"O resultado é: "<<somar(a, b)<<std::endl;
+        break;
+        case 2:
+        std::cout<<"O resultado é: "<<subtrair(a, b)<<std::endl;
+        break;
+        case 3: 
+        std::cout<<"O resultado é: "<<multiplicar(a, b)<<std::endl;
+        break;
+        case 4:
+        std::cout<<"O resultado é: "<<dividir(a, b)<<std::endl;
+        break;
+        default:
+        std::cout<<"Você digitou um valor inválido!"<<std::endl;
+    }
+}
+
 int main(){
 
-    int num1, num2, soma, subt, mult;
-    float div;
-
-    std::cout<<"Digite aqui o valor para número 1: "<<std::endl;
-    std::cin>>num1;
-
-    std::cout<<"Digite aqui o valor para número 2: "<<std::endl;
-    std::cin>>num2;
-
-
-    soma = num1 + num2;
-    subt = num1 - num2;
-    mult = num1 * num2;
-    div = num1 / num2;
-
-    std::cout<<"O resultado da soma é: "<<soma<<std::endl;
-    std::cout<<"O resultado da subtração é: "<<subt<<std::endl;
-    std::cout<<"O resultado da multiplicação é: "<<mult<<std::endl;
-    std::cout<<"O resultado da divisão é: "<<div<<std::endl;
-
+    float num1 = 0, num2 = 0;
+    exibirResultado(num1, num2);
 
     return 0;
 }
